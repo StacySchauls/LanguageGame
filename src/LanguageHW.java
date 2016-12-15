3ee44c90c31a31e30377bc60b210fe773beac194
Stacy <Stacy@DESKTOP-50AU5UD>
2016-12-14 17:18:59 -0800

    Final

--- src/LanguageHW.java
+++ src/LanguageHW.java
@@ -1,32 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.io.*;
 import java.sql.*;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
- * @author Stacy
+ *  Class to implement a simple GUI to match and search languages with
+ * their first 10 numbers.
+ * @author Stacy Schauls    
+ * @version Challenge
  */
-public class LanguageHW 
+public class LanguageHW implements ActionListener
 {
-    private List LNames, Words;
-    private JLabel Language, Values, output;
-    private JButton search;
+    //fields used
+    private WsuWindow win;
+    private List LNames, Words, LNames2, Words2, Random;
+    private JLabel Language, Values, output, output2, good, language2, value2;
+    private JButton search, select, check;
     private java.sql.Statement stmt = null;
+    private Map<String,Map<Integer, String>> map;
+    private String lang;
+    private int meaning, randomNum;
+       
     /**
      * Constructor, creates our window with the proper components
      */
     public LanguageHW()
-    {   //create the window and the components that go inside it
-        WsuWindow win = new WsuWindow();
+    {  
+        
+        //create the window and the components that go inside it
+        //MINIMAL VERSION
+        win = new WsuWindow();
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //scrolling list for the languages and values
         //language
@@ -59,9 +65,65 @@ public class LanguageHW
         //add the components to the window
         win.add(LNames); win.add(Words);  win.add(Language); win.add(Values);
         win.add(search); win.add(output);
+        //STANDARD VERSION
+        //buttons
+        select = new JButton("Select");
+        select.setSize(70,30);
+        select.setLocation(20, 160);
+        win.add(select);
+        
+        check = new JButton("Check");
+        check.setSize(80,30);
+        check.setLocation(280,160);
+        win.add(check);
+        
+        
+        //output label
+        output2 = new JLabel();
+        output2.setFont(new Font("Arial", Font.BOLD, 16));
+        output2.setSize(70,20);
+        output2.setLocation(20, 200);
+        output2.setHorizontalAlignment(0);
+        win.add(output2);
+        
+        good = new JLabel();
+        good.setFont(new Font("Arial", Font.BOLD, 16));
+        good.setSize(110,30);
+        good.setLocation(265,200);
+        good.setHorizontalAlignment(0);
+        win.add(good);
+        
+        //lists
+        LNames2 = new List();
+        LNames2.setSize(90,80);
+        LNames2.setLocation(110,160);
+        win.add(LNames2);
+        
+        Words2 = new List();
+        Words2.setSize(40,80);
+        Words2.setLocation(220,160);
+        win.add(Words2);
+        //labels
+        language2 = new JLabel("Language");
+        language2.setSize(115,20);
+        language2.setLocation(115,140);
+        win.add(language2);
+        
+        value2 = new JLabel("Value");
+        value2.setSize(100,20);
+        value2.setLocation(225,140);
+        win.add(value2);
+        //separation rectangle
+        WsuRectangle rect = new WsuRectangle();
+        rect.setSize(355,3);
+        rect.setLocation(15,130);
+        win.add(rect);
+  
+        Random = new List();
         win.repaint();
         this.Database();
-        this.application();
+        this.mapping();
+        
     }
     
     /**
@@ -106,6 +168,7 @@ public class LanguageHW
         
         //the queries
         String sql = "SELECT * FROM Language";
+        String sql2 = "SELECT word FROM Vocab";
         
         //run the queries
         try
@@ -117,11 +180,19 @@ public class LanguageHW
                 //add the languages to the language list
                 String lname = rs.getString("langName");
                 LNames.add(lname);
+                LNames2.add(lname);
+            }
+            java.sql.ResultSet rs2 = stmt.executeQuery(sql2);
+            while(rs2.next())
+            {
+                String word = rs2.getString("word");
+                Random.add(word);
             }
             //add 1-10 ti tge value box
-            for(int i = 0; i<10; i++)
+            for(int i = 1; i<11; i++)
                 {
-                    Words.add(i+"");
+                   // Words.add(i+"");
+                    Words2.add(i+"");
                 }
         }catch(Exception e)
         {
@@ -130,15 +201,20 @@ public class LanguageHW
         }
     }
     
-   public void application()
+    /**
+     * Method to map the values 
+     */
+    public void mapping()
    {
-        Map<String,Map<Integer, String>> map;
-        map = new HashMap<String, Map<Integer,String>>();
+       //create the map
+         map = new HashMap<String, Map<Integer,String>>();
         String sql = "SELECT langname, meaning_id, word FROM Vocab "
                 + "INNER JOIN Language ON language.ID = Vocab.lang_id";
+        //run the query 
      try
      {
         java.sql.ResultSet rs = stmt.executeQuery(sql);
+        
         while(rs.next())
         {
             String langName = rs.getString("langname");
@@ -151,20 +227,61 @@ public class LanguageHW
             }
             map.get(langName).put(meaning_id, word);
         }
-        System.out.println(map.size());
+       
         
      }catch(Exception e)
-     {
-         System.err.println("App query failed: "+e.getMessage());
-         return;
-     }
+        {
+            System.err.println("App query failed: "+e.getMessage());
+            return;
+        }
+     //Actionlisteners for each componenet as needed
+    LNames.addActionListener(this);
+    search.addActionListener(this);
+    select.addActionListener(this);
+    check.addActionListener(this);
      
-    
    }
   
-   
-   
-   
+  /**
+     * Method for the action listener for each button clicked
+     * @param the action event
+     */
+    public void actionPerformed(ActionEvent e) 
+    {
+        if(e.getSource().equals(LNames))
+        {
+              
+              lang = LNames.getSelectedItem();
+             Words.clear();
+              
+//            meaning = Words.getSelectedIndex();
+//            output.setText(this.searching(lang, meaning));
+              for(int i=0; i<10; i++)
+              {
+                 Words.add(map.get(lang).get(i));
+              }   
+            win.repaint();
+        }else if(e.getSource().equals(select))
+        {
+            java.util.Random random = new java.util.Random();
+            randomNum = random.nextInt(199-0) + 0;
+            System.out.println(randomNum);
+            output2.setText(Random.getItem(randomNum));
+            
+        }else if(e.getSource().equals(check))
+        {
+            lang = LNames2.getSelectedItem();
+            meaning = Words2.getSelectedIndex();
+            System.out.println(lang+" "+ meaning);
+            if(map.get(lang).get(meaning).equals(Random.getItem(randomNum)))
+            {
+                good.setText("Good");
+            }else
+            {
+                good.setText("Bad");
+            }
+        }
+    }
    
     /**
      * @param args the command line arguments
@@ -173,5 +290,4 @@ public class LanguageHW
     {
         LanguageHW lang = new LanguageHW();
     }
-    
 }
